8d79214eaefd6cf4d79886a190bbba4e75bce01d
Stack <stack@ilpdev.com>
2022-02-21 10:53:22 -0800

    fix: :bug: change scrublands terrain color

--- prs-mapper.lua
+++ prs-mapper.lua
@@ -1,4 +1,4 @@
--- Procedural Realms Mapping Script for Mudlet v0.2.0
+-- Procedural Realms Mapping Script for Mudlet v0.2.1
 -- by Stack (https://ilpdev.com/prs) based on generic GMCP mapping script 
 -- by Blizzard (https://worldofpa.in) based upon an MSDP script from the Mudlet
 -- forums in the generic mapper thread with pieces from Jor'Mox's generic mapper
@@ -27,7 +27,7 @@ local terrain_types = {
     ["Bridge"] = {id = 20, r = 255, g = 255, b = 255},
     ["Road"] = {id = 21, r = 0, g = 128, b = 128},
     ["Steppe"] = {id = 22, r = 255, g = 255, b = 0},
-    ["Scrublands"] = {id = 22, r = 255, g = 255, b = 0},
+    ["Scrublands"] = {id = 19, r = 128, g = 128, b = 0},
 }
 
 -- list of possible movement directions and appropriate coordinate changes
