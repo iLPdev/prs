cbcc1e52e3b10b51a07c025fde439be3261fac3b
Stack <stack@ilpdev.com>
2022-02-27 05:51:21 -0800

    feat: :bug: add Farmland terrain color

--- prs-mapper.lua
+++ prs-mapper.lua
@@ -1,4 +1,4 @@
--- Procedural Realms Mapping Script for Mudlet v0.6.0
+-- Procedural Realms Mapping Script for Mudlet v0.7.0
 -- by Stack (https://ilpdev.com/prs) based on generic GMCP mapping script 
 -- by Blizzard (https://worldofpa.in) based upon an MSDP script from the Mudlet
 -- forums in the generic mapper thread with pieces from Jor'Mox's generic mapper
@@ -31,7 +31,9 @@ local terrain_types = {
     ["Wasteland"] = {id = 24, r = 128, g = 0, b = 0},
     ["Fence"] = {id = 25, r = 255, g = 255, b = 255},
     ["River"] = {id = 26, r = 0, g = 0, b = 255},
-    ["Lake"] = {id = 27, r = 0, g = 0, b = 255},  
+    ["Lake"] = {id = 27, r = 0, g = 0, b = 255},
+    ["Fishing Pier"] = {id = 28, r = 0, g = 0, b = 255},
+    ["Farmland"] = {id = 29, r = 255, g = 255, b = 0},
 }
 
 -- list of possible movement directions and appropriate coordinate changes
