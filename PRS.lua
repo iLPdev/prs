af05d0925eeb9794bc2b8ebe32beb3c54a836362
Stack <stack@ilpdev.com>
2022-05-05 13:19:30 -0700

    feat(install): :package: remove generic mapper if installed
    generic_Mapper is installed by default with mudlet. it doesn't work with PR, so there's no need for it

--- PRS.lua
+++ PRS.lua
@@ -1,5 +1,10 @@
 -- Procedural Realms Script (PRS) by Stack
-local version = "0.10.2"
+local version = "0.11.0"
+
+-- check if the generic_mapper package is installed and, if so, uninstall it
+if table.contains(getPackages(),"generic_mapper") then
+  uninstallPackage("generic_mapper")
+end
 
 -- Open the windows
 PRSchat.tabs()
