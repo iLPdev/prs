21e157a0f3de3a9e4f3b7f9ea422c50ab2196ab9
iLPdev <stack@ilpdev.com>
2023-06-07 10:33:47 -0700

    feat(stats): :wastebasket: deprecate initial stats handler

--- PRS.lua
+++ PRS.lua
@@ -1,16 +1,14 @@
 -- Procedural Realms Script (PRS) by Stack and Dalem
+
 local version = "1.7.0"
 
 -- check if the generic_mapper package is installed and, if so, uninstall it
-if table.contains(getPackages(), "generic_mapper") then
-    uninstallPackage("generic_mapper")
+if table.contains(getPackages(),"generic_mapper") then
+  uninstallPackage("generic_mapper")
 end
 
 -- Open the windows
 PRSchat.tabs()
-registerAnonymousEventHandler("gmcp.Char.player", function()
-    PRSstats.stats()
-end, true)
 
 -- set Discord button to PR invite
 setDiscordGameUrl("https://discord.gg/rRFEjc6vV9", "Procedural Realms")
