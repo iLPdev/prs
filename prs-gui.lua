c17ef5d41564f87011d4012c3388a22b8589ea55
grrtt49 <81640993+grrtt49@users.noreply.github.com>
2023-08-17 16:00:08 -0600

    Squashed commit of the following:
    commit 2298fe4fe693d8026fc072f1404cd1e34d6d4140
    Author: grrtt49 <81640993+grrtt49@users.noreply.github.com>
    Date:   Thu Aug 17 06:59:44 2023 -0600
    
        Added skills tab (#134)
    
        * Added skills tab
    
        Added a skills list with gauges for level-based skills. Includes colors based on skill type (weapon, crafting, artisan, and combat)
    
        * fixed update bug
    
    commit 7861f74772b68af72798afa43ac05a908b0a989a
    Author: grrtt49 <81640993+grrtt49@users.noreply.github.com>
    Date:   Thu Aug 17 06:55:27 2023 -0600
    
        feat(quests): Add Quests tab (#132)
    
        * Added quests tab
    
        Quest tab complete with colors, progress bars, and updating on gmcp changes
    
        * removed unnecessary code

--- prs-gui.lua
+++ prs-gui.lua
@@ -113,7 +113,7 @@ GUI.tabwindow1 = GUI.tabwindow1 or Adjustable.TabWindow:new({
     inactiveTabFGColor = "#555555",
     color1 = "rgb(24,24,28)",
     color2 = "rgb(16,16,20)",
-    tabs = {"Vitals", "Stats"}
+    tabs = {"Vitals", "Stats", "Skills"}
 }, GUI.left_top)
 
 GUI.tabwindow2 = GUI.tabwindow2 or Adjustable.TabWindow:new({
@@ -127,7 +127,7 @@ GUI.tabwindow2 = GUI.tabwindow2 or Adjustable.TabWindow:new({
     inactiveTabFGColor = "#555555",
     color1 = "rgb(24,24,28)",
     color2 = "rgb(16,16,20)",
-    tabs = {"Combat"}
+    tabs = {"Combat", "Quests"}
 }, GUI.left_bottom)
 
 GUI.tabwindow3 = GUI.tabwindow3 or Adjustable.TabWindow:new({
